fdf8c965490c682065d228dec324f5e70727455d
mazimpakar <rosemazimpaka2@gmail.com>
2019-03-04 16:49:31 +0200

    deployment to heroku

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
-
+ 
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:123456@localhost/blogs'
